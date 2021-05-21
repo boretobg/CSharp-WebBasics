9941d5254d61d63f0a2023011d0796845d7d7194
Bobby <boretobg@gmail.com>
2021-05-22 01:14:55 +0300

    add exercies not finished

---
+++ WebServer-AsynchronousProcessing/Exercies/Chronometer/Chronometer/Models/Chronometer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chronometer
+{
+    public class Chronometer : IChronometer
+    {
+        private float milliseconds = 0;
+        private int seconds = 0;
+        private int minutes = 0;
+        private long current = 0;
+
+        public string GetTime => throw new NotImplementedException();
+
+        public List<string> Laps => throw new NotImplementedException();
+
+        public string Lap()
+        {
+            return $"{minutes}:{seconds}:{current}";
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            var stopwatch = new Stopwatch();
+
+            while (true)
+            {
+                while (seconds < 60)
+                {
+                    while (milliseconds < 1000)
+                    {
+                        stopwatch.Start();
+                        milliseconds++;
+                        current = stopwatch.ElapsedMilliseconds;
+                    }
+                    stopwatch.Restart();
+
+                    Thread.Sleep(1000);
+                    seconds++;
+                    milliseconds = 0;
+                }
+                
+                minutes++;
+                seconds = 0;
+            }
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebServer-AsynchronousProcessing/Exercies/Chronometer/Chronometer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Chronometer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chronometer = new Chronometer();
+
+            Console.WriteLine("Start or exit?");
+            string command = String.Empty;
+
+            while (true)
+            {
+                command = Console.ReadLine();
+                if (command.ToLower() == "start")
+                {
+                    var task = Task.Run(chronometer.Start);
+                    break;
+                }
+                else if (command.ToLower() == "exit")
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            //while (true)
+            //{
+            //    command = Console.ReadLine();
+
+            //    if (command.ToLower() == "lap")
+            //    {
+            //        string lapResult = chronometer.Lap();
+            //        Console.WriteLine(lapResult);
+            //    }
+            //}
+
+            while (true)
+            {
+                Console.WriteLine(chronometer.Lap());
+            }
+        }
+    }
+}
