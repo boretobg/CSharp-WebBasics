f5718dc17d507ff6ac259474e4b3ddb3bbc1b427
Bobby <boretobg@gmail.com>
2021-05-31 15:20:44 +0300

    add httpcookie classes

---
+++ SIS/SIS.HTTP/Cookies/HttpCookieCollection.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SIS.HTTP.Cookies
+{
+    public class HttpCookieCollection : IHttpCookieCollection
+    {
+        private ICollection<HttpCookie> cookieList;
+
+        public HttpCookieCollection()
+        {
+            this.cookieList = new List<HttpCookie>();
+        }
+
+
+        public ICollection<HttpCookie> CookieList => this.cookieList;
+
+        public void AddCookie(HttpCookie cookie)
+        {
+            cookieList.Add(cookie);
+        }
+
+        public bool ContainsCookie(string key)
+        {
+            var tempCookie = cookieList.FirstOrDefault(x => x.Key == key);
+
+            if (tempCookie is null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public HttpCookie GetCookie(string key)
+        {
+            var targetCookie = cookieList.FirstOrDefault(x => x.Key == key);
+
+            return targetCookie;
+        }
+
+        public bool HasCookies()
+        {
+            if (cookieList.Any())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        //public override string ToString()
+        //{
+        //   return string.Join(HttpCookieStringSeparator, this.cookies.Values);
+        //}
+    }
+}
