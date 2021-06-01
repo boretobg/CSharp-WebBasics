3dbb3e41974cbb89610c2f1df3e70f13c6efb4af
Bobby <boretobg@gmail.com>
2021-06-01 14:20:13 +0300

    add web server

---
+++ Workshop/MyWebServer/WebServer/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace WebServer
+{
+    using System.Threading.Tasks;
+    public class Program
+    {
+        public static async Task Main()
+        {
+            var server = new HttpServer("127.0.0.1", 9090);
+
+            await server.Start();
+        }
+    }
+}
