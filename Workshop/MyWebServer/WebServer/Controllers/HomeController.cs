eb96c2be8e08f57ac4f9d71da12aa785fe1e8a6c
Bobby <boretobg@gmail.com>
2021-06-06 23:01:19 +0300

    add controllers

---
+++ Workshop/MyWebServer/WebServer/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿namespace WebServer.Controllers
+{
+    using WebServer.Server.Controllers;
+    using WebServer.Server.Http;
+
+    public class HomeController : Controller
+    {
+        public HomeController(HttpRequest request) : base(request)
+        {
+        }
+
+        public HttpResponse Index() => Text("Hellp from Bobby!");
+        public HttpResponse LocalRedirect() => Redirect("/Cats");
+        public HttpResponse ToSoftuni() => Redirect("https://softuni.bg");
+    }
+}
