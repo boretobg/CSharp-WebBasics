1d67b79bff8efacdf529f18b1c9a12b05e871031
Bobby <boretobg@gmail.com>
2021-06-08 23:44:54 +0300

    update

--- Workshop/MyWebServer/WebServer.Server/Responses/ContentResponse.cs
+++ Workshop/MyWebServer/WebServer.Server/Responses/ContentResponse.cs
@@ -1,21 +1,10 @@
 ﻿namespace WebServer.Server.Responses
 {
-    using System.Text;
-    using WebServer.Server.Common;
     using WebServer.Server.Http;
 
     public class ContentResponse : HttpResponse
     {
-        public ContentResponse(string text, string contentType) : base(HttpStatusCode.OK)
-        {
-            Guard.AgainstNull(text);
-
-            var contentLenght = Encoding.UTF8.GetByteCount(text).ToString();
-
-            this.Headers.Add("Content-Type", contentType);
-            this.Headers.Add("Content-Lenght", contentLenght);
-
-            this.Content = text;
-        }
+        public ContentResponse(string content, string contentType) : base(HttpStatusCode.OK)
+            => this.PrepareContent(content, contentType);
     }
 }
--- Workshop/MyWebServer/WebServer/StartUp.cs
+++ Workshop/MyWebServer/WebServer/StartUp.cs
@@ -6,10 +6,7 @@
     public class StartUp
     {
         public static async Task Main()
-        {
-            var server = new HttpServer("127.0.0.1", 9090);
-
-            await server.Start();
-        }
+            => await new HttpServer(routes => routes
+            .MapGet<HomeController>("/", c => c.Index()));
     }
 }
