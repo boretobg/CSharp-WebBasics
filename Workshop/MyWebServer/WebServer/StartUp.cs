77b5365d9d663ed277c9c9ca1ef5198561dbd9e1
Bobby <boretobg@gmail.com>
2021-06-12 17:11:20 +0300

    changes

--- Workshop/MyWebServer/WebServer/StartUp.cs
+++ Workshop/MyWebServer/WebServer/StartUp.cs
@@ -3,20 +3,16 @@
     using System.Threading.Tasks;
     using WebServer.Controllers;
     using WebServer.Server;
+    using WebServer.Server.Controllers;
 
-    public class StartUp
-    {
-        public static async Task Main()
-           => await new HttpServer(routes => routes
-                .MapGet<HomeController>("/", c => c.Index())
-                .MapGet<HomeController>("/ToCats", c => c.LocalRedirect())
-                .MapGet<HomeController>("/Softuni", c => c.ToSoftUni())
-                .MapGet<AnimalsController>("/Cats", c => c.Cats())
-                .MapGet<AnimalsController>("/Dogs", c => c.Dogs())
-                .MapGet<AnimalsController>("/Bunnies", c => c.Bunnies())
-                .MapGet<AnimalsController>("/Turtles", c => c.Turtles())
-                .MapGet<CatsController>("/Cats/Create", c => c.Create())
-                .MapPost<CatsController>("/Cats/Save", c => c.Save()))
-            .Start();
-    }
+    public class Startup
+        {
+            public static async Task Main()
+                => await new HttpServer(routes => routes
+                    .MapStaticFiles()
+                    .MapControllers()
+                    .MapGet<HomeController>("/ToCats", c => c.LocalRedirect()))
+                .Start();
+        }
+  
 }
